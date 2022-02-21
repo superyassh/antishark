65c5f176f22865133a2af31575ef75d6470ad50d
superyassh <yanshukishore@gmail.com>
2022-02-21 13:29:41 +0530

    comit to master

--- smart-contract/factory.js
+++ smart-contract/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x2Bec4B5E67FE9e6Ba5768D83d49a71A60067B813"
+  "0x08400f51ba2c71980d66611f5593021deD52dd02"
 );
 
 export default instance;
