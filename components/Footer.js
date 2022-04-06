2c052d9b9550ede2d0392d9f7430ad036a22aa5c
superyassh <yanshukishore@gmail.com>
2022-04-06 08:53:35 +0530

    New Shit~

--- components/Footer.js
+++ components/Footer.js
@@ -109,7 +109,7 @@ export default function Footer() {
           justify={{ base: "center", md: "space-between" }}
           align={{ base: "center", md: "center" }}
         >
-          <Text>🌐 Made while 💩 by Yanshu & Aditya</Text>
+          <Text>🌐 Made while 💩 by Yanshu</Text>
           <Stack direction={"row"} spacing={6}>
             {/* <SocialButton
             label={"Website"}
